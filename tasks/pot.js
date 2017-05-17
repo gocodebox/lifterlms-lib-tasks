18b80423949e0b55cf6aac4c64fb790294715f1d
Thomas Patrick Levy <thomasplevy@gmail.com>
2017-05-17 12:02:36 -0700

    pot task

---
+++ tasks/pot.js
@@ -0,0 +1,25 @@
+module.exports = function( gulp, config ) {
+
+	var   sort  = require( 'gulp-sort' )
+		, wpPot = require( 'gulp-wp-pot' )
+	;
+
+	gulp.task( 'pot', function() {
+
+		gulp.src( [ '*.php', './**/*.php', '!vendor/*' ] )
+
+			.pipe( sort() )
+
+			.pipe( wpPot( {
+				bugReport: config.pot.bugReport,
+				domain: config.pot.domain,
+				package: config.pot.package,
+				lastTranslator: config.pot.lastTranslator,
+				team: config.pot.team,
+			} ) )
+
+			.pipe( gulp.dest( config.pot.dest ) )
+
+	} );
+
+};
