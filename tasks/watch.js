cdfe24d1fa6467fe712276862e60e6042b20f58f
Thomas Patrick Levy <thomasplevy@gmail.com>
2017-10-31 11:05:06 -0700

    watching will check textdomain before generating a pot

--- tasks/watch.js
+++ tasks/watch.js
@@ -2,7 +2,7 @@ module.exports = function( gulp, config ) {
 
   gulp.task( 'watch', function() {
 
-    gulp.watch( [ '*.php', './**/*.php', '!vendor/*' ], [ 'pot' ] );
+    gulp.watch( [ '*.php', './**/*.php', '!vendor/*' ], [ 'textdomain', 'pot' ] );
     gulp.watch( config.scripts.watch, [ 'scripts' ] );
     gulp.watch( config.styles.watch, [ 'styles' ] );
 
