aaf2f120267e3f5c2b6d7320d17dc9ef05241bbc
Thomas Patrick Levy <thomasplevy@gmail.com>
2018-05-02 14:34:48 -0700

    custom watcher

--- tasks/watch.js
+++ tasks/watch.js
@@ -2,6 +2,16 @@ module.exports = function( gulp, config ) {
 
   gulp.task( 'watch', function() {
 
+    if ( config.watch.custom.length ) {
+
+      config.watch.custom.forEach( function( obj ) {
+
+        gulp.watch( obj.glob, obj.tasks );
+
+      } );
+
+    }
+
     gulp.watch( [ '*.php', './**/*.php', '!vendor/*' ], [ 'textdomain', 'pot' ] );
     gulp.watch( config.scripts.watch, [ 'scripts' ] );
     gulp.watch( config.styles.watch, [ 'styles' ] );
