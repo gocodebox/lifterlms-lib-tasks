3083d51427083bb07bcc3323baf750886b9b6d56
Thomas Patrick Levy <thomasplevy@gmail.com>
2018-08-14 10:15:04 -0700

    fix prerelease suffix replacements

--- tasks/versioner.js
+++ tasks/versioner.js
@@ -38,10 +38,18 @@ module.exports = function( gulp, config ) {
       } ) )
       // filter to the main plugin file & perform more replacements there
       .pipe( main_filter )
-      .pipe( replace( / \* Version: (\d+\.\d+\.\d+)(\-\D+\.\d+)?/g, function( match, p1, offset, string ) {
+      .pipe( replace( / \* Version: (\d+\.\d+\.\d+)(\-\D+\.\d+)?/g, function( match, p1, p2, string ) {
+        // if there's a prerelease suffix (eg -beta.1) remove it entirely
+        if ( p2 ) {
+          match = match.replace( p2, '' );
+        }
         return match.replace( p1, the_version );
       } ) )
-      .pipe( replace( /public \$version = '(\d+\.\d+\.\d+)(\-\D+\.\d+)?';/g, function( match, p1, offset, string ) {
+      .pipe( replace( /public \$version = '(\d+\.\d+\.\d+)(\-\D+\.\d+)?';/g, function( match, p1, p2, string ) {
+        // if there's a prerelease suffix (eg -beta.1) remove it entirely
+        if ( p2 ) {
+          match = match.replace( p2, '' );
+        }
         return match.replace( p1, the_version );
       } ) )
       // restore all files filtered out
