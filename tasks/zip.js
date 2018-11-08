691baf0062cede9eba867b59aaf337f25c9b5447
Thomas Patrick Levy <thomasplevy@gmail.com>
2018-11-08 10:29:11 -0800

    exclude composer directory from build when composer isn't configured

--- tasks/zip.js
+++ tasks/zip.js
@@ -7,12 +7,17 @@ module.exports = function( gulp, config, args ) {
 
   gulp.task( 'zip', function( cb ) {
 
+    var build_src = config.zip.src.default.concat( config.zip.src.custom );
+
     // if configured, run composer to install only prod. deps.
     if ( config.zip.composer ) {
       composer( 'install --no-dev --no-scripts', { async: false } );
+    // if not configured, exclude composer from the build
+    } else {
+      build_src.concat( '!./vendor/**' );
     }
 
-    return gulp.src( config.zip.src.default.concat( config.zip.src.custom ) )
+    return gulp.src( build_src )
       // add the src to an unsuffixed directory (eg: lifterlms/)
       .pipe( rename( function( path ) {
         path.dirname = config.zip.name + '/' + path.dirname;
