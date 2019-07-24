a25269937b86966e5a14a406a3444a17311f86f9
James Richards <GitHub@happycorner.us>
2019-07-24 12:55:29 -0500

    Rename files (#7)
    * Rename pot:js.js to pot-js.js
    
    * Rename pot:php.js to pot-php.js
    
    * Rename styles:rtl.js to styles-rtl.js
    
    * Update references to renamed files.

--- tasks/build.js
+++ tasks/build.js
@@ -1,6 +1,6 @@
 module.exports = function( gulp, config ) {
 
-  var tasks = [ 'textdomain', 'pot', 'scripts', 'styles', 'styles:rtl' ].concat( config.build.custom );
+  var tasks = [ 'textdomain', 'pot', 'scripts', 'styles', 'styles-rtl' ].concat( config.build.custom );
 
   gulp.task( 'build', gulp.series( ...tasks ) );
 
